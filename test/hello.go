fa35e08fbd78cf209783134b8e81df4f99ffd949
xianyu <964106410@qq.com>
2023-05-02 17:37:07 +0800

    feature-part1: Hello, World

--- test/hello.go
+++ test/hello.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const spanish = "Spanish"
+const french = "French"
+
+func Hello(name string, language string) string {
+	if "" == name {
+		return "Hello, World"
+	}
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
 func main() {
-	fmt.Println("hello, world!")
+	fmt.Println(Hello("World", ""))
 }
